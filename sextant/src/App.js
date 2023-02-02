f1114cdc5f6ff5558ed41a1d658eae96bd6c8dc8
Natborks <natborks@Natborkss-MacBook-Air.local>
2023-02-02 01:32:34 +0000

    update metric component to add API call

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -6,20 +6,6 @@ import MaxwidthWrapper from "./components/MaxwidthWrapper/MaxwidthWrapper";
 import Metric from "./components/Metric/Metric";
 
 function App() {
-  const [userIp, setUserIp] = useState({ ipv4: "", ipv6: "" });
-
-  React.useEffect(() => {
-    fetch("https://api.ipify.org?format=json")
-      .then((response) => response.json())
-      .then((data1) => {
-        fetch("https://api64.ipify.org?format=json")
-          .then((response) => response.json())
-          .then((data) => setUserIp({ ipv4: data1.ip, ipv6: data.ip }))
-          .catch((error) => alert(error));
-      })
-      .catch((error) => console.error(error));
-  }, []);
-
   return (
     <>
       <Banner />
@@ -27,8 +13,8 @@ function App() {
       <main>
         <MaxwidthWrapper>
           <Exhibit>
-            <Metric data={userIp.ipv4} />
-            <Metric data={userIp.ipv6} ipv6 />
+            <Metric url="https://api.ipify.org?format=json" />
+            <Metric ipv6 url="https://api64.ipify.org?format=json" />
           </Exhibit>
         </MaxwidthWrapper>
       </main>
--- sextant/src/components/Metric/Metric.js
+++ sextant/src/components/Metric/Metric.js
@@ -2,12 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Metric.module.css";
 
-function Metric({ data, ipv6 = false }) {
+function Metric({ url, ipv6 = false }) {
+  const [ip, setIp] = React.useState("");
+
+  React.useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data1) => {
+        setIp(data1.ip);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   const title = ipv6 ? "User Ip (v6)" : "User Ip (v4)";
   return (
     <article className={styles.wrapper}>
       <h1>{title}</h1>
-      <p>{data}</p>
+      <p>{ip}</p>
     </article>
   );
 }
